9ec55d3e2157477088820aaf8c6231c8428260b7
Niklas Correa <niklasb2@illinois.edu>
2018-02-06 13:51:44 -0500

    Class WebSearch       main method empty       urlToString method completed

--- src/WebSearch.java
+++ src/WebSearch.java
@@ -6,20 +6,72 @@ public class WebSearch {
 
     public static void main (String[] args) {
 
+       // System.out.println(urlToString("http://erdani.com/tdpl/hamlet.txt"));
+
+        System.out.println("=============================================================================================");
+
+        System.out.println("Word Count: ");
+        System.out.println(totalWordCount("one two three"));
+        System.out.println("Chosen Word Count: ");
+        System.out.println(chosenWordCount("Get thee to a nunnery", "thee"));
     }
 
     public static String urlToString (final String url) {
 
         Scanner urlScanner;
+
         try {
 
             urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+
         } catch (IOException e) {
 
             return "";
         }
+
         String contents = urlScanner.useDelimiter("\\A").next();
         urlScanner.close();
+
         return contents;
+    }
+
+    public static int totalWordCount (final String stream) {
+
+        int wordCount = 0;
+
+        String[] streamLine = stream.split("\n");
+        String[] streamText;
+
+        for (int i = 0; i <= streamLine.length - 1; i++) {
+
+            streamText = streamLine[i].split(" ");
+
+            wordCount = streamText.length;
+        }
+
+        return wordCount;
+    }
+
+    public static int chosenWordCount (final String stream, final String chosen) {
+
+        int chosenWordCount = 0;
+
+        String[] streamLine = stream.split("\n");
+        String[] streamText;
+
+        for (int i = 0; i <= streamLine.length - 1; i++) {
+
+            streamText = streamLine[i].split(" ");
+
+            for (int j = 0; j <= streamText.length - 1; j++) {
+
+                if (streamText[j].equals(chosen)) {
+
+                    chosenWordCount++;
+                }
+            }
+        }
+
+        return chosenWordCount;
     }
 }
